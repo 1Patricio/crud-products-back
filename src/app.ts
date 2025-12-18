2064f3a6d3b78b5e163218297bbd6464b8d8dcc7
Anderson Patricio <andersongpatricio@hotmail.com>
2025-12-18 12:00:32 -0300

    fix: deploy

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "cool-test-products.netlify.app/"],
+    origin: ["http://localhost:5173", "https://cool-test-products.netlify.app"],
   })
 );
 
